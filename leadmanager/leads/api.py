d629db60f4efd25adf7fba6eb38cfd83febf63cf
Titus Weller <weller_17@hotmail.com>
2021-10-28 18:48:43 -0500

    Create basic CRUD API for lead manager.

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import permission_classes
+from leads.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
